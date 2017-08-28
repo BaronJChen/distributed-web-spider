66f08c0ef6f64d9c7aa985aad209129ed2e81dcb
BaronChen <jason-null@foxmail.com>
2017-08-28 19:20:41 +0800

    Update build.sh

--- deploy/build.sh
+++ deploy/build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 mvn clean
+export PATH=$PATH:/var/jenkins_home/maven/bin
 # -pl is used to skip bootstrap module here
 # -amd is to install the dependency automically
 mvn -pl '!bootstrap' install -amd
