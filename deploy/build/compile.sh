d37eee9fbd2e8f10ead40e603896e5ebb8f293b1
BaronChen <jason-null@foxmail.com>
2017-08-29 23:32:15 +0800

    feat: add eureka k8s yaml

--- deploy/build/compile.sh
+++ deploy/build/compile.sh
@@ -5,3 +5,8 @@ export PATH=$PATH:/var/jenkins_home/maven/bin
 # -pl is used to skip bootstrap module here
 # -amd is to install the dependency automically
 mvn -pl '!bootstrap' install -amd -X
+
+# 编译并且推送镜像
+# 192.168.28.12:5000应该被写入环境变量
+docker build -f deploy/dockerfile/eureka -t='192.168.28.12:5000/eureka' .
+docker push 192.168.28.12:5000/eureka
