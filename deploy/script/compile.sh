97166ee0310ad857b4afae5ed85619fa94a9c572
chenbin <wb-cb306312@alibaba-inc.com>
2017-09-09 23:21:33 +0800

    feat: use local repository

--- deploy/script/compile.sh
+++ deploy/script/compile.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
-which mvn 
-echo $PATH
-
-mvn clean
-# maven is installed '/var/jenkins_home'
-export PATH=$PATH:/var/jenkins_home/maven/bin
 # -pl is used to skip bootstrap module here
 # -amd is to install the dependency automically
-mvn -pl '!bootstrap' install -amd
+mvn clean -pl '!bootstrap' install -amd
 
 # 编译镜像
-docker build -f deploy/dockerfile/eureka -t="$REGISTRY_NAMESPACE/eureka:$VERSION" .
-# 登录镜像站
-docker login --username=$REGISTRY_USERNAME  --password=$REGISTRY_PASSWORD $REGISTRY_ADDRESS
+docker build -f deploy/dockerfile/eureka -t="$REGISTRY/eureka:$VERSION" .
+
 # 推送镜像
-docker push $REGISTRY_NAMESPACE/eureka:$VERSION
+docker push $REGISTRY/eureka:$VERSION
