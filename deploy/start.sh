fa5910bd6d5e4163ed84e893881609b4d3a20c87
chenbin <wb-cb306312@alibaba-inc.com>
2017-09-09 16:07:06 +0800

    fix: su user

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,13 +1,6 @@
 #!/usr/bin/env bash
-REGISTRY_ADDRESS=registry.cn-hangzhou.aliyuncs.com
-REGISTRY_USERNAME=18115198992
-REGISTRY_PASSWORD=14162536binchen
-REGISTRY_NAMESPACE=registry.cn-hangzhou.aliyuncs.com/dsw
-VERSION=0.001
-WORKSPACE_HOME=`pwd`
-
 # 切换用户
-echo ' ' | su root
+echo $PASSWORD | su $USER
 
 source $WORKSPACE_HOME/deploy/script/compile.sh
 source $WORKSPACE_HOME/deploy/script/deploy.sh
