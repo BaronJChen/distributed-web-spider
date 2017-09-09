7ebf275c58ddb005095b857932ae6e7a75c4d5b4
chenbin <wb-cb306312@alibaba-inc.com>
2017-09-09 16:56:23 +0800

    feat: remove maven args -X

--- deploy/script/compile.sh
+++ deploy/script/compile.sh
@@ -7,7 +7,7 @@ mvn clean
 export PATH=$PATH:/var/jenkins_home/maven/bin
 # -pl is used to skip bootstrap module here
 # -amd is to install the dependency automically
-mvn -pl '!bootstrap' install -amd -X
+mvn -pl '!bootstrap' install -amd
 
 # 编译镜像
 docker build -f deploy/dockerfile/eureka -t="$REGISTRY_NAMESPACE/eureka:$VERSION" .
