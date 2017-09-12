07564a6d575e3eb12c4f05bcfa1eb0fa216936cc
BaronChen <jason-null@foxmail.com>
2017-09-12 23:44:57 +0800

    feat: add zipkin build

--- deploy/script/compile.sh
+++ deploy/script/compile.sh
@@ -9,9 +9,11 @@ docker build -f deploy/dockerfile/eureka -t="$REGISTRY/eureka:$VERSION" .
 docker build -f deploy/dockerfile/zuul -t="$REGISTRY/zuul:$VERSION" .
 docker build -f deploy/dockerfile/admin -t="$REGISTRY/admin:$VERSION" .
 docker build -f deploy/dockerfile/frontend -t="$REGISTRY/frontend:$VERSION" .
+docker build -f deploy/dockerfile/zipkin -t="$REGISTRY/zipkin:$VERSION" .
 
 # push images
 docker push $REGISTRY/eureka:$VERSION
 docker push $REGISTRY/zuul:$VERSION
 docker push $REGISTRY/admin:$VERSION
 docker push $REGISTRY/frontend:$VERSION
+docker push $REGISTRY/zipkin:$VERSION
